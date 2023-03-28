96d93c2dbc083dfe381828df9f27b6b093aeff7a
Danielvargas777 <juvargas92@uan.edu.co>
2023-03-27 23:01:19 -0500

    diseño de las interfaces pregunta 2 y 3

--- app/src/main/java/com/example/quien_quiere_ser_millonario/Pregunta2Activity.kt
+++ app/src/main/java/com/example/quien_quiere_ser_millonario/Pregunta2Activity.kt
@@ -1,11 +1,38 @@
 package com.example.quien_quiere_ser_millonario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Pregunta2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pregunta2)
+
+        val btn : ImageButton = findViewById(R.id.imageButton11)
+        btn.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+        }
+
+        val btn2 : ImageButton = findViewById(R.id.imageButton2)
+        btn2.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+        }
+
+
+        val btn3 : ImageButton = findViewById(R.id.imageButton9)
+        btn3.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+        }
+
+        val btn4 : ImageButton = findViewById(R.id.imageButton13)
+        btn4.setOnClickListener{
+            val intent: Intent = Intent(this,Pregunta3Activity :: class.java)
+            startActivity(intent)
+        }
     }
 }
