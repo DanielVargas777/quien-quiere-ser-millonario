e3d33de902e33f27c9c4b45d6de5e5fd6eb2a14f
Danielvargas777 <juvargas92@uan.edu.co>
2023-03-31 01:47:07 -0500

    diseño de la app completa

--- app/src/main/java/com/example/quien_quiere_ser_millonario/Pregunta5Activity.kt
+++ app/src/main/java/com/example/quien_quiere_ser_millonario/Pregunta5Activity.kt
@@ -1,11 +1,43 @@
 package com.example.quien_quiere_ser_millonario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Pregunta5Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pregunta5)
+
+        val btn : ImageButton = findViewById(R.id.imageButton28)
+        btn.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+
+
+        }
+
+        val btn2 : ImageButton = findViewById(R.id.imageButton29)
+        btn2.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+
+
+        }
+        val btn3 : ImageButton = findViewById(R.id.imageButton30)
+        btn3.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+
+
+        }
+        val btn4 : ImageButton = findViewById(R.id.imageButton31)
+        btn4.setOnClickListener{
+            val intent: Intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
